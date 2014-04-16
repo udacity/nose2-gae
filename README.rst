c27e9b5154a0c6dcbf2011bc3a930bca1800fceb
Attila-Mihaly Balazs <dify.ltd@gmail.com>
2014-04-16 15:57:23 +0300

    Updated packaging so that it works :-)

---
+++ README.rst
@@ -0,0 +1,13 @@
+nose2-gae
+=========
+
+.. image:: https://api.travis-ci.org/udacity/nose2-gae.png?branch=master
+   :height: 19px
+   :width: 77px
+   :alt: nose2-gae Travis-CI build status
+   :target: https://travis-ci.org/udacity/nose2-gae
+
+Plugin for the `nose2 testrunner <https://nose2.readthedocs.org/en/latest/>`_ to run the tests in the Google App Engine environment, similar to `NoseGAE <https://github.com/Trii/NoseGAE>`_.
+
+Currently in alpha version. The most notable missing features are the import/file sandbox and documentation :-).
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from distutils.core import setup
+
+NAME = 'nose2-gae'
+VERSION = '0.1.2'
+PACKAGES = ['nose2gae']
+DESCRIPTION = 'nose2 plugin to run the tests in the Google App Engine environment'
+URL = 'https://github.com/udacity/nose2-gae'
+LONG_DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Testing',
+]
+
+AUTHOR = 'Attila-Mihály Balázs'
+AUTHOR_EMAIL = 'dify.ltd+nose2gae@gmail.com'
+KEYWORDS = ['unittest', 'testing', 'tests', 'nose2', 'gae']
+
+params = dict(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=PACKAGES,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+)
+setup(**params)
+
